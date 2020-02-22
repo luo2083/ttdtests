c856521e5d48edc28947784e9410afe4f23842c1
luo2083 <1354029556@qq.com>
2020-02-22 19:45:16 +0800

    modify superlists/setting.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -14,26 +14,26 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if 'DJANGO_DEBUG_FALSE' in os.environ:
-    DEBUG = False 
-    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITENAME']]
-else:
-    DEBUG = True
-    SECRET_KEY = 'insecure-key-for-dev'
-    ALLOWED_HOST = []
+#if 'DJANGO_DEBUG_FALSE' in os.environ:
+#   DEBUG = False 
+#   SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+#   ALLOWED_HOSTS = [os.environ['SITENAME']]
+#else:
+#    DEBUG = True
+#   SECRET_KEY = 'insecure-key-for-dev'
+#   ALLOWED_HOST = []
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-#SECRET_KEY = 'v0&tb&xeqbdz3sh&9ivjh9g=8t7+9j#8nf27=w#0g&n62e1zi2'
+SECRET_KEY = 'v0&tb&xeqbdz3sh&9ivjh9g=8t7+9j#8nf27=w#0g&n62e1zi2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = True
 
-#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
